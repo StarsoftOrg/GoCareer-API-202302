f99fbd3e098d80b733510134950a7473572d90d8
Jack Arana <arack.dev@gmail.com>
2023-11-15 13:34:02 -0500

    chore: add swagger url

--- src/main/java/com/startsoft/gocareerapi/GocareerApiApplication.java
+++ src/main/java/com/startsoft/gocareerapi/GocareerApiApplication.java
@@ -2,12 +2,17 @@ package com.startsoft.gocareerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GocareerApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GocareerApiApplication.class, args);
+
+        String url = "http://localhost:8080/swagger-ui.html";
+        System.out.println("\n• Swagger UI is available at » " + url);
     }
 
 }
