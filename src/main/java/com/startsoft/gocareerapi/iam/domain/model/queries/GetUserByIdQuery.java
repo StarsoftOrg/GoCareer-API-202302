e1550915937218ccca799047e527059745706f0a
Jack Arana <arack.dev@gmail.com>
2023-11-16 14:18:51 -0500

    feat(domain): added commands, queries and services

---
+++ src/main/java/com/startsoft/gocareerapi/iam/domain/model/queries/GetUserByIdQuery.java
@@ -0,0 +1,4 @@
+package com.startsoft.gocareerapi.iam.domain.model.queries;
+
+public record GetUserByIdQuery(Long userId) {
+}
---
+++ src/main/java/com/startsoft/gocareerapi/iam/domain/services/UserCommandService.java
@@ -0,0 +1,39 @@
+package com.startsoft.gocareerapi.iam.domain.services;
+
+import com.startsoft.gocareerapi.iam.domain.model.aggregates.User;
+import com.startsoft.gocareerapi.iam.domain.model.commands.SignInCommand;
+import com.startsoft.gocareerapi.iam.domain.model.commands.SignUpCommand;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.Optional;
+
+/**
+ * User command service.
+ * <p>
+ *     This service is responsible for handling user aggregate commands.
+ * </p>
+ */
+public interface UserCommandService {
+
+    /**
+     * Handle the sign-up command.
+     * <p>
+     *     This method is responsible for handling the sign-up command.
+     * </p>
+     * @param command The sign-up command.
+     * @return The user aggregate.
+     * @see SignUpCommand
+     */
+    Optional<User> handle(SignUpCommand command);
+
+    /**
+     * Handle the sign-in command.
+     * <p>
+     *     This method is responsible for handling the sign-in command.
+     * </p>
+     * @param command The sign-in command.
+     * @return The user aggregate and the generated token.
+     * @see SignInCommand
+     */
+    Optional<ImmutablePair<User, String>> handle(SignInCommand command);
+}
