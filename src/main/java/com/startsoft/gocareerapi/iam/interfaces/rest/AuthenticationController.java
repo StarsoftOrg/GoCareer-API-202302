f88bf7d716845e142e4f3f44ab53ede6114c168a
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-11-24 11:46:12 -0500

    feat: Cross Origin

--- src/main/java/com/startsoft/gocareerapi/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/startsoft/gocareerapi/iam/interfaces/rest/AuthenticationController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
  * </p>
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("*")
 @RequestMapping(value = "/api/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Authentication", description = "Authentication Endpoints")
 public class AuthenticationController {
