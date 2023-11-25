5a6d6cbd0a8cba67b6de1b5007c3ece64f297918
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-11-24 19:18:35 -0500

    feat: updated origins

--- src/main/java/com/startsoft/gocareerapi/GocareerApiApplication.java
+++ src/main/java/com/startsoft/gocareerapi/GocareerApiApplication.java
@@ -23,7 +23,7 @@ public class GocareerApiApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings (CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
             }
         };
     }
