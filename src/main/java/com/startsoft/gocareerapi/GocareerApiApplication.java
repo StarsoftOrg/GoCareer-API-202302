252649e9a48001b83590609ad025b60ec86ef480
Wilver Arana R <wilver.ar.dev@gmail.com>
2024-06-15 15:56:42 -0500

    feat(deploy): update - cors origin

--- src/main/java/com/startsoft/gocareerapi/GocareerApiApplication.java
+++ src/main/java/com/startsoft/gocareerapi/GocareerApiApplication.java
@@ -25,7 +25,7 @@ public class GocareerApiApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                registry.addMapping("/**").allowedOrigins("https://go-career.web.app")
                         .allowedMethods("*").allowedHeaders("*");
             }
         };
